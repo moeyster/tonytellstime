df88a25f0b7a012789f87deabb1234a80469f5f4
Tony Moey <moeyster@gmail.com>
2014-11-06 16:13:06 +0000

    Added structure to users pages and static pages + ran bundle install with bootstrap gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Workspace::Application.routes.draw do
+
+get 'static_pages/about'
+get 'static_pages/home'
+get 'static_pages/contact'
+
+resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
